cce25ff92ba0388775a96d1f904f6637f14be4e3
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-08-31 13:45:30 +0100

    Add files via upload

---
+++ build/precache-manifest.fed34d4c296e059aa8f8063fc54ef1cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db2d974be4bfb8a8ec42ca4d6ffa3e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebcfc550074db15601f8",
+    "url": "/static/css/main.54c04cd0.chunk.css"
+  },
+  {
+    "revision": "4153f27f55aed1795934",
+    "url": "/static/js/2.55532e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55532e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebcfc550074db15601f8",
+    "url": "/static/js/main.d1db3652.chunk.js"
+  },
+  {
+    "revision": "7fe4ce60dafbd2e7ac73",
+    "url": "/static/js/runtime-main.5290079f.js"
+  }
+]);
